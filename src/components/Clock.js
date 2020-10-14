c578a4c4b94e71965f47b928641adf36e2a508cf
Steven Torrence <storrence88@gmail.com>
2020-10-13 20:16:02 -0500

    Fix formatting

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,31 +1,29 @@
-import React, { useRef, useEffect } from 'react'
-import { getTime } from '../helper'
-import styled from 'styled-components'
+import React, { useRef, useEffect } from 'react';
+import { getTime } from '../helper';
+import styled from 'styled-components';
 
 const ClockWrapper = styled.h1`
   font-size: 60px;
   font-family: Orbitron;
   letter-spacing: 7px;
-  color: #17D4FE;
+  color: #17d4fe;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
-`
+`;
 
 const Clock = () => {
   // Create reference to the span element in the DOM using useRef hook
   let timeRef = useRef(null);
 
   // Run getTime method from helper.js with useEffect hook
-  useEffect(() => { 
+  useEffect(() => {
     getTime(timeRef);
-    setInterval(() => getTime(timeRef), 1000) 
-  }, [])
+    setInterval(() => getTime(timeRef), 1000);
+  }, []);
 
-  return (
-    <ClockWrapper ref={timeRef}>00:00:00</ClockWrapper>
-  )
-}
+  return <ClockWrapper ref={timeRef}>00:00:00</ClockWrapper>;
+};
 
 export default Clock;
